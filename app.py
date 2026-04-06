2674c22cdd1c7a4d84a6c606cd58df517cdaceb0
Tanish Kumar <tanishkumar1723@gmail.com>
2026-04-06 22:09:23 +0530

    fixed indentation error

--- app.py
+++ app.py
@@ -43,9 +43,7 @@ def predict():
 
     return render_template('index.html', prediction_text=output)
 
-
- import os
-
+import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
