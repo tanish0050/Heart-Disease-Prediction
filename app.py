823daa7a8ab5c7324b78733dcae12e5d988ed85e
Tanish Kumar <tanishkumar1723@gmail.com>
2026-04-06 22:19:54 +0530

    fixed indentation final

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def predict():
     return render_template('index.html', prediction_text=output)
 
 import os
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+
+if __name__ =="__main__";
+    port = int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0", port=port)
