eef37f42949ce7b732ae8c42a221f001ac6d1b08
Tanish Kumar <tanishkumar1723@gmail.com>
2026-04-06 22:04:08 +0530

    fixed double run issue

--- app.py
+++ app.py
@@ -44,5 +44,8 @@ def predict():
     return render_template('index.html', prediction_text=output)
 
 
+ import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
