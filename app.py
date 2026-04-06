e990da64424bb6b59eb95a8362161f95b33a3ca6
Tanish Kumar <tanishkumar1723@gmail.com>
2026-04-06 23:18:47 +0530

    fixed syntax error final

--- app.py
+++ app.py
@@ -45,3 +45,25 @@ import os
 if __name__ =="__main__":
     port = int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        features = [
+            float(request.form['age']),
+            float(request.form['sex']),
+            float(request.form['cp']),
+            float(request.form['trestbps']),
+            float(request.form['chol']),
+            float(request.form['thalach'])
+        ]
+
+        final_features = [np.array(features)]
+        prediction = model.predict(final_features)
+
+        output = "Heart Disease Detected 💔" if prediction[0] == 1 else "No Heart Disease ✅"
+
+        return render_template('index.html', prediction_text=output)
+
+    except Exception as e:
+        return f"Error: {e}"
